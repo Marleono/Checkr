b5ac5eb6101399b042a5536114ad5e681c554bdf
Maria <masha-leonova@mail.ru>
2022-06-01 18:15:06 +0300

    fixed the color of the sticky nav

--- js/scrollmenu.js
+++ js/scrollmenu.js
@@ -1,12 +1,15 @@
 function scrollmenu() {
-    const navTop = document.querySelector('.nav-wrap')
-    const nav = document.querySelector('.nav-wrap')
-    window.addEventListener('scroll', () => {
-        if (nav.offsetTop > 1) {
-            nav.style.backgroundColor = 'rgba(255, 255, 255, 1)'     
-        }
+    const nav = document.querySelector('#main')
 
-    })
+        window.addEventListener('scroll', () => {
+            if (nav.offsetTop > 1) {
+                nav.style.backgroundColor = 'rgba(255, 255, 255, 1)'     
+            } else if (nav.offsetTop == 0) {
+                nav.style.backgroundColor = 'rgba(255, 255, 255, .5)'
+            }
+
+        })
 }
+   
 
 scrollmenu()
