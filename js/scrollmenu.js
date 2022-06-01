68ed06e9d67b75cbe6d902517d27dad3a532efb4
Maria <masha-leonova@mail.ru>
2022-06-01 14:15:20 +0300

    new commit

--- js/scrollmenu.js
+++ js/scrollmenu.js
@@ -1,6 +1,6 @@
 function scrollmenu() {
-    const navTop = document.querySelector('nav')
-    const nav = document.querySelector('nav')
+    const navTop = document.querySelector('.nav-wrap')
+    const nav = document.querySelector('.nav-wrap')
     window.addEventListener('scroll', () => {
         if (nav.offsetTop > 1) {
             nav.style.backgroundColor = 'rgba(255, 255, 255, 1)'     
